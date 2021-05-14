32c25c22e15e58981f8e83f38c868e5edb3277c6
LuuVanThanh <luuvanthanh121@gmail.com>
2021-05-14 23:02:03 +0700

    abc

---
+++ Payment_Service/database/migrations/2021_04_21_131712_create_cards_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('card_number',16);
+            $table->string('expire_date',4);
+            $table->string('ccv',3);
+            $table->string('card_type');
+            $table->integer('amount');
+            $table->tinyInteger('status')->default(1);
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
